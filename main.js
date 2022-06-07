b6db27762f387c2e76fdfd6226d996c28c7b7f7c
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-07 17:03:27 -0600

    testing

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ server.listen(3000, ()=>{console.log('server open')});
 server.use(express.static('../frontend'));
 server.use(express.json());
 
+//test
+
 server.post('/send', async (req, res)=>{
     console.log('got a request!');
     console.log(req.body)
