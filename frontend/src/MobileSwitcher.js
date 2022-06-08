2b8a790552eca9645e5d9dc9193bb0be5fd47e84
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-08 15:11:05 -0600

    uhhhhh

--- frontend/src/MobileSwitcher.js
+++ frontend/src/MobileSwitcher.js
@@ -1,10 +1,18 @@
-const is_mobile = navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/BlackBerry/i);
+window.onload = CheckForMobile();
 
-console.log('script ran')
 
-if (is_mobile != null){
-    document.getElementById('output').innerText = "you are on moblie"
-} else {
-    document.getElementById('output').innerText = "you are not on moblie"
-    console.log('not mobile')
+function CheckForMobile(){
+
+    const is_mobile = navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/BlackBerry/i);
+
+    console.log(navigator.userAgent)
+    console.log('script ran')
+    
+    if (is_mobile != null){
+        document.getElementById("output").innerText = "you are on moblie"
+    } else {
+        document.getElementById("output").innerText = "you are not on moblie"
+        console.log('not mobile')
+    }
+
 }
