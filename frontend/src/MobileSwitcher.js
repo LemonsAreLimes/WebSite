7bfdca4b2bcf2f08207e0e606f620d068cd5f47f
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-08 15:07:56 -0600

    fixed dom.id = null

--- frontend/src/MobileSwitcher.js
+++ frontend/src/MobileSwitcher.js
@@ -1,7 +1,10 @@
 const is_mobile = navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/BlackBerry/i);
 
+console.log('script ran')
+
 if (is_mobile != null){
     document.getElementById('output').innerText = "you are on moblie"
 } else {
     document.getElementById('output').innerText = "you are not on moblie"
+    console.log('not mobile')
 }
