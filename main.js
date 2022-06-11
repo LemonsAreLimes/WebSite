deb99199fd7b224908f7f57fc2cf4909f706e361
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-11 12:33:48 -0600

    mongo pass via env

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const fsp = require('fs/promises');
 const server = express();
 
 const PORT = process.env.PORT || 8080
+const MongoPass = process.env.MongoPass
+console.log(MongoPass);
 
 server.listen(PORT, ()=>{console.log('server open')});
 
