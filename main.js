97cba4b6600a52c22de31e1a680582c36949aa09
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-13 17:42:45 -0600

    added logging for new users

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const server = express();
 
 const PORT = process.env.PORT || 8080
 
-const MongoPass = process.env.MongoPass
+const Password = process.env.MongoPass;
+const Username = process.env.MongoUsername;
 console.log("mongo pass:" + MongoPass);
 
 server.listen(PORT, ()=>{console.log('server open')});
@@ -14,7 +16,7 @@ server.listen(PORT, ()=>{console.log('server open')});
 server.use(express.static('frontend'));
 server.use(express.json());
 
-const url = "mongodb+srv://lemons:"+MongoPass+"@atlascluster.ccoj7.mongodb.net/testing-mongo?retryWrites=true&w=majority";
+const url = "mongodb+srv://"+Username+":"+Password+"@sitelogging.ccoj7.mongodb.net/?retryWrites=true&w=majority";
 const op = { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }
 
 console.log('connecting to mongo DB...');
@@ -30,7 +32,7 @@ mongoose.connect(url, op).then(
 
 server.post('/send', async (req, res)=>{
     console.log('got a request!');
-    console.log(req.body);
+    console.log(req.body.TYPE);
 
     if(req.body.TYPE == "TEST"){
         console.log('test');
@@ -39,8 +41,37 @@ server.post('/send', async (req, res)=>{
     }
 
     if(req.body.TYPE == 'NEWUSER'){
-        console.log('test');
-        console.log(req.body);
+        console.log('New User Detected!');
+
+        //send response so the client isnt waiting on that site for too long
+        const data = {UMM:"ANYTHING"}
+        res.json(JSON.stringify(data));
+
+        //create schema
+        const LogFormat = new Schema({
+            LOCATION: String,
+            IP: String,
+            AGENT: String,
+            INTERNET_PROVIDER: String,
+        }, {timestamps: true});
+
+        //turn it into a model for some reason
+        const DataModel = mongoose.model('NewUsers', LogFormat);
+
+        //fill it in with data
+        const UserData = new DataModel({
+            LOCATION: req.body.DATA.LOCATION,
+            IP: req.body.DATA.IP,
+            AGENT: req.body.DATA.AGENT,
+            INTERNET_PROVIDER: req.body.DATA.INTERNET_PROVIDER,
+        });
+
+        //send it to mongo db
+        UserData.save().then(
+            (result)=>{
+                console.log(result);
+            }
+        )
     }
 
 });
