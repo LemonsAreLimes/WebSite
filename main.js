aadafa55932c84fc9f6b5d7f232006c779ec599b
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-13 15:03:08 -0600

    testing out logging with mongo DB

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
 const express = require('express');
-const fs = require('fs');
-const fsp = require('fs/promises');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 
 const server = express();
 
 const PORT = process.env.PORT || 8080
+
 const MongoPass = process.env.MongoPass
-console.log(MongoPass);
+console.log("mongo pass:" + MongoPass);
 
 server.listen(PORT, ()=>{console.log('server open')});
 
 server.use(express.static('frontend'));
 server.use(express.json());
 
+const url = "mongodb+srv://lemons:"+MongoPass+"@atlascluster.ccoj7.mongodb.net/testing-mongo?retryWrites=true&w=majority";
+const op = { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }
+
+console.log('connecting to mongo DB...');
+mongoose.connect(url, op).then(
+    (res) =>{
+        console.log('connected');
+    }
+).catch(
+    (err)=>{
+        console.log(err)
+    }
+);
+
 server.post('/send', async (req, res)=>{
     console.log('got a request!');
-    console.log(req.body);
-    
+
+    if(req.body.TYPE == 'TEST'){
+        console.log('test');
+        console.log(req.body);
+    }
+
 });
 
